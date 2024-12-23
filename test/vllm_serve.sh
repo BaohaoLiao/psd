a462384b4666bb9d0e0fa5af5e1f315a71c12bb8
baohaoliao <liaobaohao@gmail.com>
2024-12-23 13:27:35 +0100

    first commit

---
+++ test/vllm_serve.sh
@@ -0,0 +1,11 @@
+python -m vllm.entrypoints.openai.api_server \
+        --model /mnt/nushare2/data/baliao/PLLMs/qwen/Qwen2.5-Math-1.5B-Instruct \
+        --served-model-name Qwen2.5-Math-1.5B-Instruct \
+        --tensor-parallel-size 1 \
+        --port 8888 \
+        --host 0.0.0.0 \
+        --trust-remote-code \
+        --max-num-batched-tokens 99999 \
+        --max-num-seqs 9999 \
+        --max-model-len 32768 \
+        --enforce-eager
