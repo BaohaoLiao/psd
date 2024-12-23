62499d31dfb9f03d5def084eef0d5fadad7c73cf
baohaoliao <liaobaohao@gmail.com>
2024-12-23 14:10:25 +0100

    vllm serve

--- test/vllm_serve.sh
+++ test/vllm_serve.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+cd /data/chatgpt/data/baliao/psd/01_serve/psd
+
+hostname --ip-address
+
+export CUDA_VISIBLE_DEVICES=0
 python -m vllm.entrypoints.openai.api_server \
         --model /mnt/nushare2/data/baliao/PLLMs/qwen/Qwen2.5-Math-1.5B-Instruct \
         --served-model-name Qwen2.5-Math-1.5B-Instruct \
