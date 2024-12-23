78dc5c87df89d37ac823d4669aae8c8195b1f3ac
baohaoliao <liaobaohao@gmail.com>
2024-12-23 16:20:02 +0100

    first trial

---
+++ vllm_serve.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+hostname --ip-address
+MODEL="/mnt/nushare2/data/baliao/PLLMs/qwen/Qwen2.5-Math-1.5B-Instruct"
+MODEL_NAME="Qwen2.5-Math-1.5B-Instruct"
+
+export CUDA_VISIBLE_DEVICES=0
+python -m vllm.entrypoints.openai.api_server \
+        --model $MODEL \
+        --served-model-name $MODEL_NAME \
+        --tensor-parallel-size 1 \
+        --port 1234 \
+        --host 0.0.0.0 \
+        --trust-remote-code \
+        --max-num-batched-tokens 99999 \
+        --max-num-seqs 9999 \
+        --max-model-len 2048 \
+        --enforce-eager \
+        --enable_prefix_caching \
+        --gpu_memory_utilization 0.9
