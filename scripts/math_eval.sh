756eb120ac02f19083696afb810d53db821225fd
baohaoliao <liaobaohao@gmail.com>
2024-12-23 16:39:03 +0100

    run

---
+++ scripts/math_eval.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PROMPT_TYPE="qwen25-math-cot"
+MODEL_NAME_OR_PATH="Qwen2.5-Math-1.5B-Instruct"
+OUTPUT_DIR=${MODEL_NAME_OR_PATH}/math_eval
+IP_ADDRESS=""
+
+SPLIT="test"
+NUM_TEST_SAMPLE=-1
+
+# English open datasets
+DATA_NAME="math500"
+TOKENIZERS_PARALLELISM=false \
+python3 -u single_llm.py \
+    --data_name ${DATA_NAME} \
+    --data_dir "./external/qwen25_math_evaluation/data" \
+    --model_name_or_path ${MODEL_NAME_OR_PATH} \
+    --ip_address ${IP_ADDRESS} \
+    --output_dir ${OUTPUT_DIR} \
+    --split ${SPLIT} \
+    --prompt_type ${PROMPT_TYPE} \
+    --num_test_sample ${NUM_TEST_SAMPLE} \
+    --seed 0 \
+    --temperature 0 \
+    --n_sampling 1 \
+    --top_p 1 \
+    --start 0 \
+    --end -1 \
+    --use_vllm \
+    --save_outputs \
+    --overwrite \
