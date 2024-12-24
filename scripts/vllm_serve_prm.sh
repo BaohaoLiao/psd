8e34fb0ce48ba717adb0aba2863dde19ce456ed9
baohaoliao <liaobaohao@gmail.com>
2024-12-24 13:31:42 +0100

    prm

--- scripts/vllm_serve_prm.sh
+++ scripts/vllm_serve_prm.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 hostname --ip-address
-MODEL="/mnt/nushare2/data/baliao/PLLMs/qwen/Qwen2.5-Math-1.5B-Instruct"
-MODEL_NAME="Qwen2.5-Math-1.5B-Instruct"
+MODEL="/mnt/nushare2/data/baliao/PLLMs/skywork/Skywork-o1-Open-PRM-Qwen-2.5-1.5B"
+MODEL_NAME="Skywork-o1-Open-PRM-Qwen-2.5-1.5B"
 
 export CUDA_VISIBLE_DEVICES=0
 python -m vllm.entrypoints.openai.api_server \
         --model $MODEL \
         --served-model-name $MODEL_NAME \
         --tensor-parallel-size 1 \
-        --port 12342 \
+        --port 12343 \
         --host 0.0.0.0 \
         --trust-remote-code \
-        --max-model-len 4096 \
         --enforce-eager \
         --enable_prefix_caching \
         --gpu_memory_utilization 0.9
