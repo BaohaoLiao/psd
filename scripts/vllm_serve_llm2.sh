002daaad5ce7d7e48156810115b8ae482dfbd487
baohaoliao <liaobaohao@gmail.com>
2024-12-24 11:58:51 +0100

    serve

--- scripts/vllm_serve_llm2.sh
+++ scripts/vllm_serve_llm2.sh
@@ -4,12 +4,12 @@ hostname --ip-address
 MODEL="/mnt/nushare2/data/baliao/PLLMs/qwen/Qwen2.5-Math-1.5B-Instruct"
 MODEL_NAME="Qwen2.5-Math-1.5B-Instruct"
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 python -m vllm.entrypoints.openai.api_server \
         --model $MODEL \
         --served-model-name $MODEL_NAME \
         --tensor-parallel-size 1 \
-        --port 1234 \
+        --port 12342 \
         --host 0.0.0.0 \
         --trust-remote-code \
         --max-model-len 4096 \
